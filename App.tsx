01888e86a82eaaf55a172f5b53770d01f5fcb671
Borges10002 <borges10002@gmail.com>
2023-10-01 19:05:48 -0400

    create projetc

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from "react-native";
+
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./src/theme";
+
+import { Loading } from "@components/Loading";
+import Groups from "@screens/Groups";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
+}
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@components/Header";
+import { Container, Title } from "./styled";
+
+export default function Groups() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
