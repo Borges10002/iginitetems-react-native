aca2958c402cd27b8d1502992affd444b44f562c
Borges10002 <borges10002@gmail.com>
2023-11-03 22:01:40 -0400

    update

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 
-import { Container, Form } from "./styles";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -21,6 +27,23 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item == team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
