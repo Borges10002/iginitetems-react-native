d746bf4fe60e233669cfb6bd6ebc99b1ed41c273
Borges10002 <borges10002@gmail.com>
2023-12-03 14:48:44 -0400

    update

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useNavigation } from "@react-navigation/native";
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -8,6 +8,12 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group: "teste" });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -22,7 +28,7 @@ export function NewGroup() {
 
         <Input placeholder="Nome da turma" />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
